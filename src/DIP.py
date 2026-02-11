278d32c177aebcb066c29bf603f02763faec5fc5
amex-imd <amexpimd@gmail.com>
2026-02-11 19:39:52 +0300

    The method 'histogramEqualization' was added

--- src/DIP.py
+++ src/DIP.py
@@ -11,3 +11,11 @@ def gammaCorrection(img, gamma: float = 1.0):
     res = np.power(img.astype('float64') / 255, gamma)
     res = np.clip(res * 255, 0, 255).astype('uint8')
     return res
+
+def histogramEqualization(img):
+    numbers, _ = np.histogram(img.flatten(), bins=256)
+    CDF = numbers.cumsum() # convolution
+    totalPixels = img.shape[0] * img.shape[1]
+    NormalizedCDF = np.round((CDF - CDF.min()) * 255 / (totalPixels - CDF.min())).astype('uint8')
+    return NormalizedCDF[img]
+    
--- src/program.py
+++ src/program.py
@@ -3,8 +3,9 @@ import cv2
 import DIP
 
 def main() -> None:
-    img = cv2.imread('imgs/income.jpg')
-    res = DIP.gammaCorrection(img=img, gamma=2)
+    img = cv2.imread('imgs/income.jpg', cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite('imgs/tmp.jpg', img)
+    res = DIP.histogramEqualization(img=img)
     cv2.imwrite('imgs/outcome.jpg', res)
 
 
