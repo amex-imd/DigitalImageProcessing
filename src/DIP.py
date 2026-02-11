dfb80cfc3a6951b5752287dff9e37904cf7c2636
amex-imd <amexpimd@gmail.com>
2026-02-11 21:26:25 +0300

    The method 'meanFilterSingleChannelImages' was added

--- src/DIP.py
+++ src/DIP.py
@@ -15,8 +15,8 @@ def gammaCorrection(img, gamma: float = 1.0):
 def histogramEqualizationSingleChannelImages(img):
     # if len(img.shape) != 1: raise ValueError("TODO")
 
-    numbers, _ = np.histogram(img.flatten(), bins=256)
-    CDF = numbers.cumsum() # convolution
+    nums, _ = np.histogram(img.flatten(), bins=256)
+    CDF = nums.cumsum() # convolution
     totalPixels = img.shape[0] * img.shape[1]
     NormalizedCDF = np.round((CDF - CDF.min()) * 255 / (totalPixels - CDF.min())).astype('uint8')
     return NormalizedCDF[img]
@@ -31,3 +31,16 @@ def histogramEqualizationThreeChannelImages(img):
 
 def integralMatrix(mrx):
     return np.cumsum(np.cumsum(mrx, axis=0), axis=1)
+
+def meanFilterSingleChannelImages(img, kernel_size: int = 3, mode: str = 'edge'):
+    gap = kernel_size // 2
+    h, w = img.shape
+
+    tmp = np.pad(array=img.astype('float64'), pad_width=2*gap, mode=mode) # 2 * gap to avoid of going beyond borders of image
+    integral = integralMatrix(mrx=tmp)
+    
+    rows1, cols1 = np.ogrid[gap:h+gap, gap:w+gap]
+    rows2, cols2 = rows1+kernel_size, cols1+kernel_size
+    S = integral[rows1, cols1] + integral[rows2, cols2] - integral[rows1, cols2] - integral[rows2, cols1]
+    res = S / (kernel_size * kernel_size)
+    return res.astype('uint8') # ???
--- src/program.py
+++ src/program.py
@@ -4,21 +4,17 @@ import numpy as np
 import DIP
 
 def main() -> None:
-    img = cv2.imread('imgs/income.jpg')
-    print(img.shape)
-    res = DIP.histogramEqualizationThreeChannelImages(img=img)
-    cv2.imwrite('imgs/outcome.jpg', res)
-
+    print('Hello, world!')
+    img = cv2.imread('imgs/income.jpg', cv2.IMREAD_GRAYSCALE)
     mrx = np.array([[0, 1, 0, 0, 8],
-                    [2, 128, 5, 1, 59],
-                    [9, 0, 201, 3, 3],
-                    [4, 1, 1, 1, 0],
-                    [7, 0, 0, 0, 7]])
+                   [2, 128, 5, 1, 59],
+                   [9, 0, 201, 3, 3],
+                   [4, 1, 1, 1, 0],
+                   [7, 0, 0, 0, 7]])
+    res = DIP.meanFilterSingleChannelImages(img=img, kernel_size=5)
+    cv2.imwrite('imgs/outcome.jpg', res)   
+
     
-    ind = np.arange(5)
-    print(mrx)
-    integral = np.cumsum(np.cumsum(mrx, axis=0), axis=1)
-    print(integral)
 
 
 
