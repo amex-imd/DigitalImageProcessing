a15b9d6463a43569cd77e795726dda60c5b5bdbc
amex-imd <amexpimd@gmail.com>
2026-02-17 08:26:21 +0300

    The methods 'addUniformNoise' was added

--- src/DIP.py
+++ src/DIP.py
@@ -170,6 +170,11 @@ def addPoissonNoise(img, epp: float): # electorns per pixel
     
     return np.clip(noise / epp * 255, 0, 255).astype('uint8')
 
+def addUniformNoise(img, start: int = -10, stop: int = 10):
+    noise = np.random.uniform(low=start, high=stop, size=img.shape)
+    tmp = img.astype('float64') + noise
+    return np.clip(tmp, 0, 255).astype('uint8')
+
 def LaplaceFilterSingleChannelImages(img, kernelSize: int = 3, mode: str = 'edge', isDiagonals: bool = False):
     res = np.empty(shape=img.shape, dtype=img.dtype)
     
--- src/program.py
+++ src/program.py
@@ -6,8 +6,9 @@ import DIP
 def main() -> None:
     img = cv2.imread('imgs/income.jpg', cv2.IMREAD_GRAYSCALE)
     
-    cv2.imwrite('imgs/tmp.jpg', img) 
-    res = DIP.arithmeticMeanFilterSingleChannelImages(img, kernelSize=3)
+    res = DIP.addUniformNoise(img, -20, 20)
+    cv2.imwrite('imgs/tmp.jpg', res)
+    res = DIP.geometricMeanFilterSingleChannelImages(res, kernelSize=5)
     cv2.imwrite('imgs/outcome.jpg', res) 
     print('Hello, world!')
     
