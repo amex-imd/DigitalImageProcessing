692ed7e172b5ffa0f3dde7c1d8b4ca2c4e7a2541
amex-imd <amexpimd@gmail.com>
2026-02-17 08:35:18 +0300

    The methods 'minFilterSingleChannelImages' and 'minFilterThreeChannelsImages' were added

--- src/program.py
+++ src/program.py
@@ -4,11 +4,9 @@ import numpy as np
 import DIP
 
 def main() -> None:
-    img = cv2.imread('imgs/income.jpg', cv2.IMREAD_GRAYSCALE)
-    
-    res = DIP.addRayleighNoise(img, 100)
-    cv2.imwrite('imgs/tmp.jpg', res)
-    res = DIP.geometricMeanFilterSingleChannelImages(res, kernelSize=5)
+    img = cv2.imread('imgs/income.jpg')
+    cv2.imwrite('imgs/temp.jpg', img) 
+    res = DIP.minFilterThreeChannelsImages(img)
     cv2.imwrite('imgs/outcome.jpg', res) 
     print('Hello, world!')
     
